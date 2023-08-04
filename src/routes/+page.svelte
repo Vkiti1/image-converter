349d1aae5d03fb6773821e60c6467ffaf75e8767
Vkiti <viktor.skific@gmail.com>
2023-08-04 12:49:43 +0200

    Exctract Drag and Drop box to component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import { page } from '$app/stores';
 	import type { SubmitFunction } from '@sveltejs/kit';
 	import type { IImage } from '$lib/types';
+	import DragAndDrop from '$lib/components/DragAndDrop.svelte';
 
 	let drag = false;
 	let selectedFiles: File[] = [];
@@ -56,28 +57,7 @@
 			<option value="webp">webp</option>
 		</select>
 
-		<div>
-			<div
-				class="flex h-[200px] w-[400px] items-center justify-center rounded-xl border-2 border-dotted border-zinc-300"
-				class:border-blue-200={drag}
-				on:drop={dropHandler}
-				on:dragover={(e) => {
-					e.preventDefault();
-					drag = true;
-				}}
-				on:dragenter={() => (drag = true)}
-				on:dragleave={() => (drag = false)}
-				role="none"
-			>
-				<button
-					on:click={openFileSelect}
-					type="button"
-					class="rounded-xl border bg-zinc-200 px-4 py-2 font-semibold transition-colors hover:bg-zinc-400"
-					>Select a file</button
-				>
-			</div>
-			<p class="text-center">Drag and drop your images or select them.</p>
-		</div>
+		<DragAndDrop {drag} {dropHandler} {openFileSelect} />
 
 		<button
 			type="submit"
